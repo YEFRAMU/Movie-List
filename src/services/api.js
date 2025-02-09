913d3ed5285b444310cbf83bcf778fcde6d2d375
YEFRAMU <vinceumali81@gmail.com>
2025-02-09 21:34:05 +0800

    initial commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "2a0ddd1e646e5add4f90db28fa006a6b";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
